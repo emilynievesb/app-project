3f0069ea9261980617f21da02e20dd05350a21a5
emilynievesb <nemily1710@gmail.com>
2024-10-05 15:10:13 -0500

    feat: :sparkles: Se crea middleware de autenticación

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+import { verifyToken } from '../utils/jwt.js';
+
+export const authMiddleware = (req, res, next) => {
+    const token = req.cookies.token; // Leer el token desde la cookie
+
+    if (!token) {
+        return res.status(401).json({ message: 'No estás autenticado' });
+    }
+
+    try {
+        const user = verifyToken(token); // Verificar el token
+        req.user = user; // Almacenar el usuario verificado en la petición
+        next(); // Continuar con la siguiente función
+    } catch (error) {
+        return res.status(403).json({ message: 'Token inválido o expirado' });
+    }
+};
